e220d8e10d710aaa870be0364d38fc562179ece0
Chris <karampagiaschris@gmail.com>
2023-10-06 19:04:29 +0300

    Fix bug: css file was not served for all routes

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/assets", express.static(path.join(__dirname, "/public/stylesheets")));
 
 app.use("/", indexRouter);
 app.use("/shop", shopRouter);
